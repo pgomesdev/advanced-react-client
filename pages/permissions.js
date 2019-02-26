71cb9c3c88eb1ab5f53f6a401f5bc430a97279e4
pgomesdev <gomes.pedro15@gmail.com>
2019-02-26 19:47:33 -0300

    permissions management class

---
+++ pages/permissions.js
@@ -0,0 +1,10 @@
+import Permission from '../components/Permission'
+import PleaseSignIn from '../components/PleaseSignIn'
+
+const Permissions = props => (
+  <PleaseSignIn>
+    <Permission />
+  </PleaseSignIn>
+)
+
+export default Permissions
