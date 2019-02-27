6028fe67a642f4f2153cc062da98160df782b72f
pgomesdev <gomes.pedro15@gmail.com>
2019-02-27 18:16:22 -0300

    apollo local state queries and mutations

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,10 @@
-import { Fragment } from 'react'
+import React, { Fragment } from 'react'
 import Link from 'next/link'
+import { Mutation } from 'react-apollo'
 import NavStyles from './styles/NavStyles'
 import User from './User'
 import SignOut from './SignOut'
+import { TOGGLE_CART_MUTATION } from './Cart'
 
 const Nav = () => (
   <User>
@@ -23,6 +25,11 @@ const Nav = () => (
               <a>Account</a>
             </Link>
             <SignOut />
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {(toggleCart) => (
+                <button type='button' onClick={toggleCart}>My Cart</button>
+              )}
+            </Mutation>
           </Fragment>
         )}
         {!me && (
