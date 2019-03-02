d1958921061d4334c2bfa18acd5e50e223343772
pgomesdev <gomes.pedro15@gmail.com>
2019-03-02 16:09:25 -0300

    credit card processing with stripe checkout class

--- components/Cart.js
+++ components/Cart.js
@@ -10,6 +10,7 @@ import SickButton from './styles/SickButton'
 import CartItem from './CartItem'
 import calcTotalPrice from '../lib/calcTotalPrice'
 import formatMoney from '../lib/formatMoney'
+import TakeMyMoney from './TakeMyMoney'
 
 export const LOCAL_STATE_QUERY = gql`
   query LOCAL_STATE_QUERY {
@@ -49,7 +50,9 @@ const Cart = () => {
             </ul>
             <footer>
               <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-              <SickButton>Checkout</SickButton>
+              <TakeMyMoney>
+                <SickButton>Checkout</SickButton>
+              </TakeMyMoney>
             </footer>
           </CartStyles>
         )
