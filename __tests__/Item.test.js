8de27d2d1d8673acd0ecdbd5c74cf6997d664d7c
pgomesdev <gomes.pedro15@gmail.com>
2019-03-04 15:42:00 -0300

    first tests and shallow rendering class

---
+++ __tests__/Item.test.js
@@ -0,0 +1,39 @@
+import ItemComponent from '../components/Item'
+import { shallow } from 'enzyme'
+import formatMoney from '../lib/formatMoney'
+
+const fakeItem = {
+  id: 'ABC123',
+  title: 'A Cool Item',
+  price: 5000,
+  description: 'This item is really cool!',
+  image: 'dog.jpg',
+  largeImage: 'largedog.jpg',
+}
+
+describe('<Item />', () => {
+  const wrapper = shallow(<ItemComponent item={fakeItem} />)
+
+  it('renders the image properly', () => {
+    const img = wrapper.find('img')
+
+    expect(img.props().src).toBe(fakeItem.image)
+    expect(img.props().alt).toBe(fakeItem.title)
+  })
+
+  it('renders the price tag and title properly', () => {
+    const PriceTag = wrapper.find('PriceTag')
+
+    expect(PriceTag.children().text()).toBe(formatMoney(fakeItem.price))
+    expect(wrapper.find('Title a').text()).toBe(fakeItem.title)
+  })
+
+  it('renders out the buttons properly', () => {
+    const buttonList = wrapper.find('.buttonList')
+
+    expect(buttonList.children()).toHaveLength(3)
+    expect(buttonList.find('Link').exists()).toBe(true)
+    expect(buttonList.find('AddToCart').exists()).toBe(true)
+    expect(buttonList.find('DeleteItem').exists()).toBe(true)
+  })
+});
